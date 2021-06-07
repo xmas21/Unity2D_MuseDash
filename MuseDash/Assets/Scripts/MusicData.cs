ab3abd32ea66ade44cf2b1f4e06cdfa561a6c66d
HWChia <66512878+xmas21@users.noreply.github.com>
2021-06-07 21:24:41 +0800

    遊戲控制器

---
+++ MuseDash/Assets/Scripts/MusicData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName ="Lobo/音樂資料",fileName ="音樂資料")]
+public class MusicData : ScriptableObject
+{
+    [Header("音樂")]
+    public AudioClip music;
+    [Header("前面等待時間"),Range(0,10)]
+    public float timeWait = 2f;
+    [Header("節點間格時間"),Range(0,10)]
+    public float interval = 1f;
+    [Header("節點移動速度"),Range(0,1000)]
+    public float speed = 300;
+
+    [Header("音樂節點")]
+    public PointType[] points;
+}
+
+public enum PointType
+{
+    none, up, down, both
+}
---
+++ MuseDash/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [Header("音樂資料")]
+    public MusicData data;
+    [Header("生成物件 : 上方")]
+    public GameObject obj_Up;
+    [Header("生成物件 : 下方")]
+    public GameObject obj_Down;
+    [Header("生成位置 : 上方")]
+    public Transform pos_Up;
+    [Header("生成位置 : 下方")]
+    public Transform pos_Down;
+
+    private AudioSource aud;
+
+    private void Start()
+    {
+        aud = GameObject.Find("遊戲 BGM").GetComponent<AudioSource>();
+        aud.clip = data.music;
+        aud.Play();
+
+        Invoke("StartMusic", data.timeWait);
+    }
+
+    private void StartMusic()
+    {
+        Instantiate(obj_Up, pos_Up);
+    }
+}
